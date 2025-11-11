ccae19bfb21ae6892856c3e64186867002c74a69
ElenaSemenova <alenasemenova9@gmail.com>
2025-11-11 14:46:50 +1100

    Add new steps for test of localisation

--- step_definitions/agifyLocalization.steps.ts
+++ step_definitions/agifyLocalization.steps.ts
@@ -25,3 +25,24 @@ Then('The country_id should be empty', function (this: CustomWorld<ILocalizedPer
 Then('The result in console log', function (this: CustomWorld<ILocalizedPerson>) {
   console.log('Full result:', JSON.stringify(this.result, null, 2));
 });
+
+When(
+  'I send a request with empty name and empty country ID',
+  async function (this: CustomWorld<ILocalizedPerson>) {
+    this.result = await this.client?.getLocalizedAgeByName('', '');
+  },
+);
+
+When(
+  'I send a request with name {string} and country_id {int}',
+  async function (this: CustomWorld<ILocalizedPerson>, name: string, country_id: string) {
+    this.result = await this.client?.getLocalizedAgeByName(name, country_id);
+  },
+);
+
+Then(
+  'The country_id should be uppercase {string}',
+  function (this: CustomWorld<ILocalizedPerson>, country_id: string) {
+    expect(this.result?.data?.country_id).to.equal(country_id.toUpperCase());
+  },
+);
