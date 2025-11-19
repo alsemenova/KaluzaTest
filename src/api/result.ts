f524719aff55c087f173122fcf15eabb6aaba510
ElenaSemenova <alenasemenova9@gmail.com>
2025-11-19 11:01:10 +1100

    Delete comment code

--- src/api/result.ts
+++ src/api/result.ts
@@ -1,17 +1,3 @@
-// export class Result {
-//   readonly isSuccessful: boolean;
-//   readonly isFailed: boolean;
-//   readonly data?: number | null;
-//   readonly error?: string;
-
-//   constructor(isSuccessful: boolean, data?: number | null, error?: string) {
-//     this.isSuccessful = isSuccessful;
-//     this.isFailed = !isSuccessful;
-//     this.data = data;
-//     this.error = error;
-//   }
-// }
-
 export class Result<T> {
   readonly isSuccessful: boolean;
   readonly isFailed: boolean;
